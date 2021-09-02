63d2171d31398ceb46df079b29f5d0ae9b30238b
Sungoh Park <spark449062@gmail.com>
2021-09-02 11:04:56 -0600

    commit tree 8ca7d1df7858f3be0cd10356ce7a204f088ba815

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+  for i in 0..(dwarves.length - 1)
+    puts "#{i + 1}. #{dwarves[i]}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |element|
+     element.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |element|
+    element.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  for i in 0..(cheese_types.length - 1)
+    for j in 0..(array.length - 1)
+      if array[j] == cheese_types[i]
+        return cheese_types[i]
+      end
+    end
+  end
+  return nil
 end
